8d4481bc353033fa33d61888abb50a0a529bffd6
sasha <pivovarao@gmail.com>
2019-06-13 15:34:33 +0300

    transferred dropdown to it's own component

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,13 +1,105 @@
 <template>
+    <div class="Dropdown">
+        <a
+            class="dropdown-trigger Dropdown__button"
+            :class="{'opened': dropdownOpened}"
+            href="#"
+            data-target="dropdown"
+            ref="dropdown"
+        >
+            USER NAME
+            <i class="Dropdown__button-icon material-icons right">arrow_drop_down</i>
+        </a>
 
+        <ul id="dropdown" class="Dropdown__list dropdown-content">
+            <li class="Dropdown__itemWrapper" v-for="item in options">
+                <router-link :to="item.url" tag="div" class="Dropdown__item">
+                    <i class="material-icons Dropdown__item-icon">{{ item.icon }}</i>{{ item.title }}
+                </router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data: () => ({
+        dropdown: null,
+        dropdownOpened: false,
+    }),
+    props: {
+        options: {
+            type: Array,
+            required: true
+        },
+    },
+    methods: {
+        logout() {
+            this.$router.push("/login?message=logout");
+        }
+    },
+    mounted() {
+        this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+            coverTrigger: false,
+            onOpenStart: () => { this.dropdownOpened = this.dropdown.isOpen },
+            onCloseStart: () => { this.dropdownOpened = this.dropdown.isOpen }
+        });
+    },
+    beforeDestroy() {
+        if (this.dropdown && this.dropdown.destroy) {
+            this.dropdown.destroy();
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.Dropdown {
+    $root: &;
 
+    &__button {
+        display: flex;
+        align-items: center;
+        transition: .3s;
+        padding: 0 10px 0 13px;
+        color: #fff;
+
+        &-icon {
+            transition: .3s;
+        }
+
+        &:hover {
+            background: #5b717b;
+        }
+
+        &.opened {
+            #{$root}__button-icon {
+                transform: rotate(180deg);
+            }
+        }
+    }
+
+    &__item {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #607d8b;
+        color: #455a64;
+        padding: 0 10px;
+        transition: .3s;
+
+        &Wrapper {
+            &:hover {
+                background: #607d8b;
+            }
+        }
+
+        &:hover {
+            color: #fff;
+        }
+
+        &-icon {
+            margin-right: 10px;
+        }
+    }
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,65 +5,41 @@
             <span class="Header__left-time">{{ date | date("datetime") }}</span>
         </div>
 
-        <ul class="right hide-on-small-and-down">
-            <li>
-                <a
-                    class="dropdown-trigger Header__dropdown-button"
-                    :class="{'opened': dropdownOpened}"
-                    href="#"
-                    data-target="dropdown"
-                    ref="dropdown"
-                >
-                    USER NAME
-                    <i class="Header__dropdown-buttonIcon material-icons right">arrow_drop_down</i>
-                </a>
-
-                <ul id="dropdown" class="Header__dropdown dropdown-content">
-                    <li class="Header__dropdown-itemWrapper">
-                        <router-link to="/profile" tag="div" class="Header__dropdown-item">
-                            <i class="material-icons Header__dropdown-itemIcon">account_circle</i>Профиль
-                        </router-link>
-                    </li>
-                    <li class="Header__dropdown-itemWrapper">
-                        <div class="Header__dropdown-item" @click="logout">
-                            <i class="material-icons Header__dropdown-itemIcon">assignment_return</i>Выйти
-                        </div>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+        <div class="Header__right">
+            <Dropdown :options="dropdownOptions"/>
+        </div>
     </nav>
 </template>
 
 <script>
+import Dropdown from "@/components/Dropdown";
 export default {
+    components: {
+        Dropdown
+    },
     data: () => ({
         date: new Date(),
         interval: null,
-        dropdown: null,
-        dropdownOpened: false,
+        dropdownOptions: [
+            {
+                title: "Профиль",
+                icon: "account_circle",
+                url: "/profile"
+            },
+            {
+                title: "Выйти",
+                icon: "assignment_return",
+                url: "/login"
+            }
+        ]
     }),
-    methods: {
-        logout() {
-            this.$router.push("/login?message=logout");
-        }
-    },
     mounted() {
-        this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
-            coverTrigger: false,
-            onOpenStart: () => { this.dropdownOpened = this.dropdown.isOpen },
-            onCloseStart: () => { this.dropdownOpened = this.dropdown.isOpen }
-        });
-
         this.interval = setInterval(() => {
             this.date = new Date();
         }, 1000)
     },
     beforeDestroy() {
         clearInterval(this.interval);
-        if (this.dropdown && this.dropdown.destroy) {
-            this.dropdown.destroy();
-        }
     },
 }
 </script>
@@ -83,53 +59,6 @@ export default {
 
         &-time {
             margin-left: 1rem;
-        }
-    }
-    
-    &__dropdown {
-        $root: &;
-
-        &-button {
-            display: flex;
-            align-items: center;
-            color: #fff;
-
-            &Icon {
-                transition: .3s;
-            }
-
-            &:hover {
-                background: #5b717b;
-            }
-
-            &.opened {
-                #{$root}-buttonIcon {
-                    transform: rotate(180deg);
-                }
-            }
-        }
-
-        &-item {
-            display: flex;
-            align-items: center;
-            border-bottom: 1px solid #5d473f;
-            color: #455a64;
-            padding: 0 10px;
-            transition: .3s;
-
-            &Wrapper {
-                &:hover {
-                    background: #607d8b;
-                }
-            }
-
-            &:hover {
-                color: #fff;
-            }
-
-            &Icon {
-                margin-right: 10px;
-            }
         }
     }
 }
